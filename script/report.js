9ce497237914a118842475a5bf49606e1c394097
Binary-Daniel <daniel.esteen.edu@rattvik.se>
2021-05-10 09:19:53 +0200

    e

---
+++ script/report.js
@@ -0,0 +1,9 @@
+function bug(){
+    alert("Mail me at daniel.esteen.edu@rattvik.se\nSet the topic to SCP BUG\nWrite wher/how to find it");
+}
+function lore(){
+    alert("Mail me at daniel.esteen.edu@rattvik.se\nSet the topic to SCP LORE\nWrite wher and what is missing");
+}
+function claim(){
+    alert("Mail me at daniel.esteen.edu@rattvik.se\nSet the topic to SCP CLAIM\nWrite what you copiright claim on this website");
+}
